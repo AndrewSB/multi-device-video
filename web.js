64a769cb69695d69ab7a9146c00b1a41377b1a21
Andrew Breckenridge <asbreckenridge@me.com>
2014-04-25 14:39:47 -0400

    cloud 9 import

--- web.js
+++ web.js
@@ -3,6 +3,8 @@ var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
 
+var rectangleOptimize = require("./rectangleOptimize.js");
+
 
 app.use(logfmt.requestLogger());
 
@@ -10,7 +12,13 @@ app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
+
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var number = Math.floor(Math.random() * (100 - 1) + 1);
+console.log(number);
+
+rectangleOptimize.printThis("hello from another file");
 });
