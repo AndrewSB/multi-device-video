55e708abc3ad43d68b28a8ceb0d1413c47b2c58c
Andrew Breckenridge <asbreckenridge@me.com>
2014-05-01 20:12:06 -0400

    Finished real init

--- youtubeFinder.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  youtubeFinder: function () {
-    var YouTubePlayer = require('youtube-player');
-    var p = new YouTubePlayer({id: 'playerElementId', width: 400, height: 300});
-    p.play('moSFlvxnbgk');
-    
-  }
-};
