59f5e1d2042d6fd5c1fa0d110d34b1a389548f8e
Andrew Breckenridge <asbreckenridge@me.com>
2014-05-07 13:03:39 -0400

    Submit to Franchi

--- client/js/client.js
+++ client/js/client.js
@@ -1,8 +1,8 @@
 var io = require("socketio");
+var video_id;
 
 function RoomController($scope) {
     var socket = io.connect();
-
     $scope.messages = [];
     $scope.roster = [];
     $scope.name = '';
@@ -14,7 +14,8 @@ function RoomController($scope) {
     });
 
     socket.on('message', function (msg) {
-        $scope.messages.push(msg);
+        //$scope.messages.push(msg);
+        $scope.messages=[msg];
         $scope.$apply();
     });
 
@@ -25,43 +26,42 @@ function RoomController($scope) {
 
     $scope.send = function send() {
         console.log('Sending message:', $scope.text);
-      
         var patt = new RegExp("v=.*");
         var regexMatch = patt.exec(String($scope.text));
         var url = String(regexMatch).substring(2);
         var video_id = url;
         var ampersandPosition = video_id.indexOf('&');
         if(ampersandPosition != -1) { video_id = video_id.substring(0, ampersandPosition); }
-        
         var videoTitle;
-
         $.getJSON('http://gdata.youtube.com/feeds/api/videos/'+ video_id +'?v=2&alt=jsonc',function(data,status,xhr){
             videoTitle = data.data.title;
             socket.emit('message', videoTitle);
-            //TODO Create new 'videoroom' with their chosen $scope.name
+            //TODO Create new 'ooroom' with their chosen $scope.name
             //TODO Redirect user to said videoroom
             $scope.text = '';
-            createRoom(String($scope.name), videoTitle);
+            createRoom(video_id);
         });
     };
 
     $scope.setName = function setName() {
         socket.emit('identify', $scope.name);
     };
-  }
+}
       
     function getScreenDimensions(){
         var height = screen.height;
         var width =  screen.width;
         alert("dimensions are " + height + "x" + width);
     }
-    function createRoom(roomName, videoID) {
-        socket.join(String(roomName));
-        //socket.join(roomName);
+    
+    function createRoom(videoID) {
+        video_id = String(videoID);
+        alert("Video ID: " + video_id);
     }
-        
+    
     function joinRoom(){
-        //TODO socket.join
         //TODO refresh page/redirect
-        alert('Ayyyy room hit');
+        var location = window.location+"?v="+video_id;
+        window.location = location;
     }
+    
